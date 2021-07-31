fc032cb1e3746ca7645c0ac124e346a0a2a7da21
Agorist-Action <agorist_action@protonmail.com>
2021-07-31 09:53:38 -0500

    Remove LoggingCS/CommonCS Path to .csproj

---
+++ TradingEngineCommonCS/UpdateResponseEnumeration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradingEngineCommonCS
+{
+    public enum UpdateType
+    {
+        OrderNew,
+        OrderCancel,
+    }
+
+    public enum ResponseType
+    {
+        OrderNewStatus,
+        OrderCancelStatus,
+        OrderFillStatus,
+    }
+}
